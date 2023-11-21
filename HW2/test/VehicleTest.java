ad75af7bfc259dc20351d101cdd25d0fdfd6d3bc
ilyanik77 <110525890+ilyanik77@users.noreply.github.com>
2023-11-21 14:58:47 +0300

    Add change VehicleTest.java

--- HW2/test/VehicleTest.java
+++ HW2/test/VehicleTest.java
@@ -5,28 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class VehicleTest {
 
-    // проверка того, что экземпляр объекта Car также является экземпляром транспортного средства; (instanceof)
+    // Экземпляр объекта Car также является экземпляром транспортного средства; (instanceof)
     @Test
     public void testCarIsInstanceOfVehicle() {
         Car car = new Car("Dodge", "Ram", 2010);
         assertTrue(car instanceof Vehicle);
     };
 
-    // проверка того, объект Car создается с 4-мя колесами
+    // Объект Car создается с 4-мя колесами
     @Test
     public void checkNumWheelsOfCar() {
         Car car = new Car("Dodge", "Ram", 2010);
         assertThat (car.getNumWheels()).isEqualTo(4);
     }
 
-    // проверка того, объект Motorcycle создается с 2-мя колесами
+    // Объект Motorcycle создается с 2-мя колесами
     @Test
     public void checkNumWheelsOfMotorcycle() {
         Motorcycle motorcycle = new Motorcycle("Yamaha", "Super Moto", 2013);
         assertThat (motorcycle.getNumWheels()).isEqualTo(2);
     }
 
-    //проверка того, объект Car развивает скорость 60 в режиме тестового вождения (testDrive())
+    // Объект Car развивает скорость 60 в режиме тестового вождения (testDrive())
     @Test
     public void checkSpeedOfCar(){
         Car car = new Car("Dodge", "Ram", 2010);
@@ -34,7 +34,7 @@ public class VehicleTest {
         assertThat(car.getSpeed()).isEqualTo(60);
     }
 
-    // проверка того, объект Motorcycle развивает скорость 75 в режиме тестового вождения (testDrive())
+    // Объект Motorcycle развивает скорость 75 в режиме тестового вождения (testDrive())
     @Test
     public void checkSpeedOfMotorcycle(){
         Motorcycle motorcycle = new Motorcycle("Yamaha", "Super Moto", 2013);
@@ -42,8 +42,8 @@ public class VehicleTest {
         assertThat(motorcycle.getSpeed()).isEqualTo(75);
     }
 
-    // проверить, что в режиме парковки (сначала testDrive, потом park,
-    // т.е эмуляция движения транспорта) машина останавливается (speed = 0)
+    // Проверка режима парковки (сначала testDrive, потом park)
+    // т.е имитация движения транспорта до остановки (speed = 0)
     @Test
     public void checkSpeedParkOfCar(){
         Car car = new Car("Dodge", "Ram", 2010);
@@ -52,8 +52,8 @@ public class VehicleTest {
         assertThat(car.getSpeed()).isEqualTo(0);
     }
 
-    // проверить, что в режиме парковки (сначала testDrive, потом park
-    // т.е эмуляция движения транспорта) мотоцикл останавливается (speed = 0)
+    // Проверка режима парковки (сначала testDrive, потом park)
+    // т.е имитация движения транспорта до остановки (speed = 0)
     @Test
     public void checkSpeedParkOfMotorcycle(){
         Motorcycle motorcycle = new Motorcycle("Yamaha", "Super Moto", 2013);
