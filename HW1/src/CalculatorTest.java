bc8a1cdbd0414a34cbf054dbb68fc3b85e25421c
ilyanik77 <110525890+ilyanik77@users.noreply.github.com>
2023-11-21 14:49:55 +0300

    Update CalculatorTest.java

--- HW1/src/CalculatorTest.java
+++ HW1/src/CalculatorTest.java
@@ -11,14 +11,14 @@ public static void main(String[] args) {
         assertThat(Calculator.calculation(2, 7, '*')).isEqualTo(14);
         assertThat(Calculator.calculation(100, 50, '/')).isEqualTo(2);
 
-        // Проверка ожидаемого исключения, с использованием утверждений AssertJ:
+        // Исключение, с использованием утверждений AssertJ:
         assertThatThrownBy(() ->
                 Calculator.calculation(8, 4, '_')
         ).isInstanceOf(IllegalStateException.class);
 
 
 
-        // проверка метода вычисления суммы покупки со скидкой
+        // Вычисление суммы покупки со скидкой
         // 1. Отрицательная сумма покупки
         assertThatThrownBy(() ->
                 Calculator.calculateDiscount(-1000, 50))
@@ -42,12 +42,7 @@ public static void main(String[] args) {
         assertThat(Calculator.calculateDiscount(1000, 25)).isEqualTo(750);
         assertThat(Calculator.calculateDiscount(1000, 100)).isEqualTo(0);
         assertThat(Calculator.calculateDiscount(1000, 0)).isEqualTo(1000);
-        /** отрицательная скидка */
-         //assertThat(Calculator.calculateDiscount(1000, -10)).isEqualTo(1000);
-        /** отрицательная сумма покупки */
-       // assertThat(Calculator.calculateDiscount(- 1000, 25)).isEqualTo(1000);
-        /** скидка больше 100 */
-       // assertThat(Calculator.calculateDiscount(1000, 101)).isEqualTo(1000);
+        
     }
 
  
